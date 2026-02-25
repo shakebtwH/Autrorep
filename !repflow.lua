68200dc4957e3a1f38a958bc3b1e657b5fdae53e
shakebtwH <neverlosesakaet@gmail.com>
2026-02-25 21:52:36 +0300

    Update !repflow.lua

--- !repflow.lua
+++ !repflow.lua
@@ -10,7 +10,7 @@ if not samp then samp = {} end
 
 local IniFilename = 'RepFlowCFG.ini'
 local new = imgui.new
-local scriptver = "4.12 | Premium"
+local scriptver = "4.13 | Premium"
 
 ---------------------------
 -- AUTO UPDATE SYSTEM
@@ -882,3 +882,4 @@ end)
 
 function showInfoWindow() info_window_state[0] = true end
 function showInfoWindowOff() info_window_state[0] = false end
+
